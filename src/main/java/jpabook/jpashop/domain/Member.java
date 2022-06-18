52b3656e9e0572cdecb27fd794d0061cafde239d
최병민 <89714987+men16922@users.noreply.github.com>
2022-06-18 18:45:55 +0900

    init

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,7 +3,10 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * packageName    : jpabook.jpashop.domain
